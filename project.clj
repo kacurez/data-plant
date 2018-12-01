1861904d881473e24e86d61fac0e11b3d4d30de6
Tomas Kacur <tomas.kacur@keboola.com>
2018-12-01 19:39:35 +0100

    setup native-image compliation with graalvm

--- project.clj
+++ project.clj
@@ -4,8 +4,23 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/core.async "0.4.474"]
                  [org.clojure/test.check "0.9.0"]
-                 [org.clojure/tools.cli "0.3.5"]]
-  :main kacurez.data-plant.cli.core
-  :jvm-opts ["-Xmx256m"])
+                 [org.clojure/tools.cli "0.4.1"]]
+  :main ^:skip-aot kacurez.data-plant.cli.core
+  :plugins [[io.taylorwood/lein-native-image "0.3.0"]]
+  :target-path "target/%s"
+  :native-image {:opts ["--verbose" "--no-server" "--enable-url-protocols=http,https" "-H:ReflectionConfigurationFiles=reflection.json" "--report-unsupported-elements-at-runtime"]
+                 :name "data-plant"}
+
+  ;; optionally set profile-specific :native-image overrides
+  :profiles {
+             :test    ;; e.g. lein with-profile +test native-image
+             {:native-image {:opts ["--report-unsupported-elements-at-runtime"
+                                    "--verbose"]}}
+
+             :uberjar ;; used by default
+             {:aot :all
+              :native-image {:opts ["-Dclojure.compiler.direct-linking=true"]}}}
+
+  ;; :jvm-opts ["-Xmx256m"]
+  )
