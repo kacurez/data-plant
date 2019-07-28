32924f24ff8399731181d9fbb5b86804f686c0ed
Tomas Kacur <tomas.kacur@keboola.com>
2019-07-28 19:03:36 +0200

    add csv cli options for enclosure and delimiter

--- src/kacurez/data_plant/cli/csv.clj
+++ src/kacurez/data_plant/cli/csv.clj
@@ -6,7 +6,7 @@
               [kacurez.data-plant.size-parser :as size-parser]
               [kacurez.data-plant.map-generator-builder :refer [parse-functions-map]]))
 
-(def usage
+(defn usage [options-summary]
   (->> ["Usage: data-plant csv size definition-map"
         "Generate a csv file to std out limited by size and defined by definition-map."
         ""
@@ -16,33 +16,38 @@
         "  1KB  - generate 1 kilobyte file"
         "  2GB  - generate 2 gigabytes file"
         ""
-        "definition-map: key value pair where key is column name and value is value"
+        "definition-map: sequence of key value pairs where key is column name and value is value"
+        ""
+        "Options:"
+        options-summary
         ""]
        (string/join \newline)))
 
 (def cli-options
-  [["-h" "--help"]])
+  [["-h" "--help"]
+   ["-e" "--enclosure ENCLOSURE" "csv enclosure" :default "\""]
+   ["-d" "--delimiter DELIMITER" "csv delimiter" :default ","]])
 
-(defn run [parsed-size parsed-definition-map]
+(defn run [parsed-size parsed-definition-map options]
   (write-csv-from-maps
    *out*
    #(random-map-from-functions-map parsed-definition-map)
    (map str (keys parsed-definition-map))
    parsed-size
-   {:delimiter "," :enclosure "\""}))
+   options))
 
-(defn prepare-run-command [size-cli-arg definition-cli-arg]
+(defn prepare-run-command [size-cli-arg definition-cli-arg options]
   (let [parsed-size (size-parser/parse size-cli-arg)
         parsed-definition (parse-functions-map definition-cli-arg)]
-    {:run #(run parsed-size parsed-definition)}))
+    {:run #(run parsed-size parsed-definition options)}))
 
 (defn parse-args [args]
-  (let [{:keys [options arguments errors]} (parse-opts args cli-options)
+  (let [{:keys [options arguments errors summary]} (parse-opts args cli-options)
         size (first arguments)
         definition (second arguments)]
     (cond
       (:help options)
-      {:exit-message usage}
+      {:exit-message (usage summary)}
 
       errors ; errors => exit with description of errors
       {:exit-message (string/join \newline errors)}
@@ -50,6 +55,6 @@
       (and
        (some? size)
        (some? definition))
-      (prepare-run-command size definition)
+      (prepare-run-command size definition options)
 
-      :else {:exit-message usage})))
+      :else {:exit-message (usage summary)})))
