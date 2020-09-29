3382c5736fe0f9580c8f6463a4572d35e4d7e846
Tomas Kacur <tomaskacur@Tomass-MacBook-Pro.local>
2020-09-29 21:40:45 +0200

    wip copy csv

--- src/kacurez/data_plant/csv/transform.clj
+++ src/kacurez/data_plant/csv/transform.clj
@@ -4,29 +4,32 @@
             [kacurez.data-plant.csv.transducers :refer [maps-to-csv-lines]]
             [clojure.java.io :as io]))
 
-(defn csv-data->maps [csv-data]
+(defn csv-data->maps [csv-data csv-header]
   (map zipmap
-       (->> (first csv-data) ;; First row is the header
-            #_(map keyword) ;; make columns as keywords
-            repeat)
-       (rest csv-data)))
+       (repeat csv-header)
+       csv-data))
 
 (defn printxf
   ([] (printxf identity))
   ([fnx]
    (map (fn [x] (println (fnx x)) x))))
 
+(defn set-column [column value]
+  (map #(assoc % column value)))
+
 (defn copy-csv [to]
   (with-open [reader (io/reader (java.util.zip.GZIPInputStream. (io/input-stream "test3.gz")))
               writer (io/output-stream to)]
     (let [csv-seq (read-csv reader)
-          csv-header (first csv-seq)]
+          csv-header (first csv-seq)
+          csv-data (rest csv-seq)]
       (transduce-to-stream writer
                            (comp
+                            (eval (read-string "(set-column \"aaa\" \"33\")"))
                             #_(printxf #(nil? (% "column4")))
                             ;; (filter #(not-empty (% "column4")))
-                            ;; (map #(dissoc % "column3"))
-                            ;; (map #(assoc % "anewColumn" "1"))
+                            ;; (map #(dissoc % \"column3\"))
+                            ;; (map #(assoc % "anewColumn" "2"))
                             (maps-to-csv-lines csv-header "," "\""))
-                           (csv-data->maps csv-seq)
+                           (csv-data->maps csv-data csv-header)
                            false))))
