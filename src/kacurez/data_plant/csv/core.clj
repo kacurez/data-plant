8db13585118b1e9731fa91633e343b0506a55162
Tomas Kacur <tomaskacur@Tomass-MacBook-Pro.local>
2020-10-04 22:44:18 +0200

    rename var

--- src/kacurez/data_plant/csv/core.clj
+++ src/kacurez/data_plant/csv/core.clj
@@ -6,21 +6,21 @@
              [transduce-coll->stream transduce-file->stream]]))
 
 (defn generate-random-csv-to-stream
-  [parsed-size output-stream definition-xf
+  [parsed-size output-stream transformation-xf
    {:keys [delimiter enclosure gzip?]
     :or {delimiter "," enclosure "\"" gzip? false}}]
   (let [size-limit-xf (:xform parsed-size)
         csv-xf (maps-to-csv-lines delimiter enclosure)
-        xf (comp definition-xf csv-xf size-limit-xf)]
+        xf (comp transformation-xf csv-xf size-limit-xf)]
     (transduce-coll->stream output-stream xf (repeat {}) gzip?)))
 
 (defn transduce-csv-file->stream
-  [csv-file-path output-stream definition-xf {:keys [delimiter enclosure gzip?]
-                                              :or {delimiter "," enclosure "\"" gzip? false}}]
+  [csv-file-path output-stream transformation-xf {:keys [delimiter enclosure gzip?]
+                                                  :or {delimiter "," enclosure "\"" gzip? false}}]
   (let
    [delimiter-char (-> delimiter char-array first)
     enclosure-char (-> enclosure char-array first)
     csv-xf (maps-to-csv-lines delimiter enclosure)
-    xf (comp (str-colls-to-csv-maps) definition-xf csv-xf)
+    xf (comp (str-colls-to-csv-maps) transformation-xf csv-xf)
     split-to-csv-lines-reader-fn (fn [input-reader] (read-csv input-reader :separator delimiter-char :quote enclosure-char))]
     (transduce-file->stream csv-file-path output-stream xf split-to-csv-lines-reader-fn gzip?)))
